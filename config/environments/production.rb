9b1f60646c313886687a2bcc415b6b6fd5988415
Nick Amabile <amabile.n@gmail.com>
2012-02-03 19:10:33 -0800

    add assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ SampleApp::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += [ 'blueprint/ie.css' ]
+  config.assets.precompile += [ 'blueprint/ie.css', 'blueprint/screen.css', 'blueprint/print.css' ]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
